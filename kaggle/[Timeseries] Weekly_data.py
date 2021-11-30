5db1b4b389cfa81a534deebabe259c760292df40
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-01 00:05:15 +0900

    Update [Timeseries] Weekly_data.py

--- kaggle/[Timeseries] Weekly_data.py
+++ kaggle/[Timeseries] Weekly_data.py
@@ -3,6 +3,7 @@
 주어진 데이터(basic2.csv)에서 주 단위 Sales의 합계를 구하고,
 가장 큰 값을 가진 주와 작은 값을 가진 주의 차이를 구하시오(절대값)
 
+dataset : basic2.csv
 https://www.kaggle.com/agileteam/bigdatacertificationkr/tasks?taskId=6930
 '''
 
