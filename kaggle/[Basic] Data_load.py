7660a6abea0eb035e3408bbd034da71fbe85b7cf
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-01 20:44:27 +0900

    Create [Basic] Data_load.py

---
+++ kaggle/[Basic] Data_load.py
@@ -0,0 +1,8 @@
+# csv 파일 불러오기
+df = pd.read_csv("../input/bigdatacertificationkr/basic1.csv")
+
+# json 파일 불러오기
+df = pd.read_json("../input/bigdatacertificationkr/basic1_json.json")
+
+# xml 파일 불러오기
+df = pd.read_xml("../input/bigdatacertificationkr/basic1_xml.xml")
