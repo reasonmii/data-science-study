17bdd46af082a91429567e3f1b6efad990bb5040
YUNA <79252563+reasonmii@users.noreply.github.com>
2022-02-11 09:37:14 +0900

    Update 10_문자열.r

--- certificate_ADP/10_문자열.r
+++ certificate_ADP/10_문자열.r
@@ -2,13 +2,18 @@
 # 길이
 nchar("ABC")   # 3
 
-# 연결
+# 문자 붙이기
+A <- paste("a","b","c",sep="-")  # "a-b-c"
+paste(A,c("e","f"))              # "a-b-c e" "a-b-c f"
+paste(A,10,sep="")               # "a-b-c10"
+
 paste("abc","def",sep="-")  # "abc-def"
 paste("the pi is approximately", pi)
 paste(v1, "loves me", collapse=", and ")
 # "1 loves me, and 2 loves me, and 3 loves me, and 4 loves me"
 
 # 하위 문자열 추출
+substr("Bigdataanalysis",1,4)  # Bigd
 substr("statistics",1,4)  # stat
 
 # 구분자로 문자열 추출
@@ -20,3 +25,4 @@ strsplit("abc,def",",")   # "abc" "def"
 
 # 쌍별 조합
 # mat <- outer(문자열1, 문자열2, paste, sep="")
+
