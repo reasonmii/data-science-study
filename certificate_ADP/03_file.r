4c87ee2c9e425a86911a517d85573bd2531ca674
YUNA <79252563+reasonmii@users.noreply.github.com>
2022-02-17 18:58:03 +0900

    Update 03_file.r

--- certificate_ADP/03_file.r
+++ certificate_ADP/03_file.r
@@ -10,5 +10,15 @@
 # 파일 불러오기
 # load("파일.R")
 # source("파일.R")
+# pdf("파일.pdf")
+#
+# sink(file, append, split)
+#  - R 코드 실행 결과를 특정 파일에서 출력
+#  - file : 출력할 파일명
+#  - append : 파일에 결과를 덮어쓰거나 추가 (default: FALSE)
+#  - split : 출력파일에만 출력(F)하거나 콘솔창에 출력(T) (default: FALSE)
+#
+# 파일 닫기
+# dev.off()
 #=====================================
 
